c6dbf0b9ac3825ef5ae3d8d86ba577d544265aeb
endy mimi <ighobuendy@gmail.com>
2025-05-30 19:52:49 -0700

    features:fixes

--- client/src/propertiesfeatures/PopularProp.jsx
+++ client/src/propertiesfeatures/PopularProp.jsx
@@ -28,7 +28,7 @@ const PopularProp = () => {
     <img className='w-full h-auto object-cover'
       src="https://res.cloudinary.com/ds0a0s3k3/image/upload/v1748388760/houseimage10_gy4koe.png"
       alt="House-Image" />
-      <div className='absolute text-white top-68 left-5 font-outfit shadow-md tracking-widest'>
+      <div className='absolute text-white bottom-15 left-5 font-outfit  shadow-md tracking-widest'>
         <h3 className='text-xl font-semibold'>Semi Detached Duplex</h3>
         <h4 className='text-xl font-semibold'>₦ 1,430,000,000</h4>
         <p className='text-[#FFFFFF] text-[14.79px]'>6 Bed | 3 Bath | 720 sqft </p>
@@ -45,7 +45,7 @@ const PopularProp = () => {
     <img className='w-full h-auto object-cover'
       src="https://res.cloudinary.com/ds0a0s3k3/image/upload/v1748388760/houseimage11_bnuu5s.png"
       alt="House-Image" />
-      <div className='absolute text-white top-68 left-5 font-outfit shadow-md tracking-widest'>
+      <div className='absolute text-white bottom-15 left-5 font-outfit shadow-md tracking-widest'>
         <h3 className='text-xl font-semibold'>Special Duplex</h3>
         <h4 className='text-xl font-semibold'>₦ 670,000,000</h4>
         <p className='text-[#FFFFFF] text-[14.79px]'>6 Bed | 3 Bath | 720 sqft </p>
@@ -58,7 +58,7 @@ const PopularProp = () => {
     <img className='w-full h-auto object-cover'
       src="https://res.cloudinary.com/ds0a0s3k3/image/upload/v1748388759/houseimage12_pvtnmy.png"
       alt="House-Image" />
-      <div className='absolute text-white top-68 left-5 font-outfit shadow-md tracking-widest'>
+      <div className='absolute text-white bottom-15 left-5 font-outfit shadow-md tracking-widest'>
         <h3 className='text-xl font-semibold'>Split-Level House</h3>
         <h4 className='text-xl font-semibold'>₦ 340,000,000</h4>
         <p className='text-[#FFFFFF] text-[14.79px]'>6 Bed | 3 Bath | 720 sqft </p>
@@ -71,7 +71,7 @@ const PopularProp = () => {
     <img className='w-full h-auto object-cover'
       src="https://res.cloudinary.com/ds0a0s3k3/image/upload/v1748388759/houseimge13_npzur6.png"
       alt="House-Image" />
-       <div className='absolute text-white top-68 left-5 font-outfit shadow-md tracking-widest'>
+       <div className='absolute text-white bottom-15 left-5 font-outfit shadow-md tracking-widest'>
         <h3 className='text-xl font-semibold'>Twin Duplex</h3>
         <h4 className='text-xl font-semibold'>₦ 290,000,000</h4>
         <p className='text-[#FFFFFF] text-[14.79px]'>6 Bed | 3 Bath | 720 sqft </p>
@@ -79,7 +79,7 @@ const PopularProp = () => {
            <MdLocationOn className="mr-1" /> Victoria island,Lagos.
         </div>
         <div>
-        <img className='absolute right-2 top-1/2 transform -translate-y-1/2 w-6 h-6' src={arrowright} alt="arrow-icon" />
+        <img className='absolute right-10 top-1/2 transform -translate-y-1/2 w-6 h-6' src={arrowright} alt="arrow-icon" />
       </div>
       </div>
       
